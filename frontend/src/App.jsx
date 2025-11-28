bc841c217a8e78327e6cc1e4c8c63fb8f5f64bd9
Abhishek Survase <abhishek.survase2000@gmail.com>
2025-11-28 22:25:16 +0530

    added .env variables for routing links

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,28 +6,49 @@ import { BookOpen, Search, Plus, Edit, List } from "lucide-react";
 import { Navigate, BrowserRouter, Routes, Route } from "react-router";
 import AllStudentPage from "./pages/AllStudentPage.jsx";
 import AddStudentPage from "./pages/AddStudentPage.jsx";
-import GetStudentPage from "./pages/GetStudentPage.jsx";
+import GetStudentPage from "./pages/FindStudentPage.jsx";
 import UpdateStudentPage from "./pages/UpdateStudentPage.jsx";
 import Dashboard_new from "./pages/Dashboard_dummy.jsx";
 import Dashboard from "./pages/Dashboard.jsx";
+import Layout_with_sidebar from "./components/layout_with_sidebar.jsx";
 
 function App() {
+  // console.log(import.meta.env.VITE_DASHBOARD_URL);
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/">
-          <Route index element={<Navigate to="dashboard" replace />} />
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="dashboard_new" element={<Dashboard_new />} />
+        <Route element={<Layout_with_sidebar />}>
+          <Route path="/">
+            <Route
+              index
+              element={
+                <Navigate to={import.meta.env.VITE_DASHBOARD_URL} replace />
+              }
+            />
+            <Route
+              path={import.meta.env.VITE_DASHBOARD_URL}
+              element={<Dashboard />}
+            />
+          </Route>
+          <Route path={import.meta.env.VITE_ALL_STUDENT_URL}>
+            <Route index element={<AllStudentPage />} />
+            <Route
+              path={import.meta.env.VITE_FIND_STUDENT_URL}
+              element={<GetStudentPage />}
+            />
+          </Route>
+          <Route path={import.meta.env.VITE_ADMISSION_URL}>
+            <Route
+              path={import.meta.env.VITE_ADD_STUDENT_URL}
+              element={<AddStudentPage />}
+            />
+            <Route
+              path={import.meta.env.VITE_UPDATE_STUDENT_URL}
+              element={<UpdateStudentPage />}
+            />
+          </Route>
         </Route>
-        <Route path="students">
-          <Route index element={<AllStudentPage />} />
-          <Route path="findStudent" element={<GetStudentPage />} />
-        </Route>
-        <Route path="admission">
-          <Route path="addStudent" element={<AddStudentPage />} />
-          <Route path="updateStudent" element={<UpdateStudentPage />} />
-        </Route>
+        <Route path="dashboard_new" element={<Dashboard_new />} />
       </Routes>
     </BrowserRouter>
   );
