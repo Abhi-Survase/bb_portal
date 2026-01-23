40f268c3fc53efec807993c52d42ca8a170e7c42
Abhishek Survase <abhishek.survase2000@gmail.com>
2026-01-24 01:22:56 +0530

    Blocks Template added to dashboard page

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -5,6 +5,7 @@ import {
   Users,
   UserPlus,
   Search,
+  TrendingDown,
   Bell,
   GraduationCap,
   FileText,
@@ -34,11 +35,65 @@ import {
   SidebarHeader,
   SidebarTrigger,
 } from "@/components/ui/sidebar";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 import { Input } from "@/components/ui/input";
 
 function Dashboard() {
+  const invoices = [
+    {
+      invoice: "INV001",
+      paymentStatus: "Paid",
+      totalAmount: "$250.00",
+      paymentMethod: "Credit Card",
+    },
+    {
+      invoice: "INV002",
+      paymentStatus: "Pending",
+      totalAmount: "$150.00",
+      paymentMethod: "PayPal",
+    },
+    {
+      invoice: "INV003",
+      paymentStatus: "Unpaid",
+      totalAmount: "$350.00",
+      paymentMethod: "Bank Transfer",
+    },
+    {
+      invoice: "INV004",
+      paymentStatus: "Paid",
+      totalAmount: "$450.00",
+      paymentMethod: "Credit Card",
+    },
+    {
+      invoice: "INV005",
+      paymentStatus: "Paid",
+      totalAmount: "$550.00",
+      paymentMethod: "PayPal",
+    },
+    {
+      invoice: "INV006",
+      paymentStatus: "Pending",
+      totalAmount: "$200.00",
+      paymentMethod: "Bank Transfer",
+    },
+    {
+      invoice: "INV007",
+      paymentStatus: "Unpaid",
+      totalAmount: "$300.00",
+      paymentMethod: "Credit Card",
+    },
+  ];
   return (
-    <div className="flex-1 flex flex-col">
+    <div className="@container/main flex-1 flex flex-col">
       <header className="h-16 bg-white border-b border-slate-200 flex items-center justify-between px-8 sticky top-0 z-10">
         <h1 className="text-xl font-semibold text-slate-800 flex items-center gap-4">
           <SidebarTrigger />
@@ -66,6 +121,168 @@ function Dashboard() {
           </div>
         </div>
       </header>
+      <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+        <div className="*:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card dark:*:data-[slot=card]:bg-card grid grid-cols-1 gap-4 px-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:shadow-xs lg:px-6 @xl/main:grid-cols-2 @5xl/main:grid-cols-4">
+          <Card className="@container/card">
+            <CardHeader>
+              <CardDescription>Total Revenue</CardDescription>
+              <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
+                $1,250.00
+              </CardTitle>
+              <CardAction>
+                <Badge variant="outline">
+                  <TrendingUp />
+                  +12.5%
+                </Badge>
+              </CardAction>
+            </CardHeader>
+            <CardFooter className="flex-col items-start gap-1.5 text-sm">
+              <div className="line-clamp-1 flex gap-2 font-medium">
+                Trending up this month <TrendingUp className="size-4" />
+              </div>
+              <div className="text-muted-foreground">
+                Visitors for the last 6 months
+              </div>
+            </CardFooter>
+          </Card>
+          <Card className="@container/card">
+            <CardHeader>
+              <CardDescription>New Customers</CardDescription>
+              <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
+                1,234
+              </CardTitle>
+              <CardAction>
+                <Badge variant="outline">
+                  <TrendingDown />
+                  -20%
+                </Badge>
+              </CardAction>
+            </CardHeader>
+            <CardFooter className="flex-col items-start gap-1.5 text-sm">
+              <div className="line-clamp-1 flex gap-2 font-medium">
+                Down 20% this period <TrendingDown className="size-4" />
+              </div>
+              <div className="text-muted-foreground">
+                Acquisition needs attention
+              </div>
+            </CardFooter>
+          </Card>
+          <Card className="@container/card">
+            <CardHeader>
+              <CardDescription>Active Accounts</CardDescription>
+              <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
+                45,678
+              </CardTitle>
+              <CardAction>
+                <Badge variant="outline">
+                  <TrendingUp />
+                  +12.5%
+                </Badge>
+              </CardAction>
+            </CardHeader>
+            <CardFooter className="flex-col items-start gap-1.5 text-sm">
+              <div className="line-clamp-1 flex gap-2 font-medium">
+                Strong user retention <TrendingUp className="size-4" />
+              </div>
+              <div className="text-muted-foreground">
+                Engagement exceed targets
+              </div>
+            </CardFooter>
+          </Card>
+          <Card className="@container/card">
+            <CardHeader>
+              <CardDescription>Growth Rate</CardDescription>
+              <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
+                4.5%
+              </CardTitle>
+              <CardAction>
+                <Badge variant="outline">
+                  <TrendingUp />
+                  +4.5%
+                </Badge>
+              </CardAction>
+            </CardHeader>
+            <CardFooter className="flex-col items-start gap-1.5 text-sm">
+              <div className="line-clamp-1 flex gap-2 font-medium">
+                Steady performance increase <TrendingUp className="size-4" />
+              </div>
+              <div className="text-muted-foreground">
+                Meets growth projections
+              </div>
+            </CardFooter>
+          </Card>
+        </div>
+        <div className="flex flex-col">
+          <div className="grid grid-cols-1 gap-4 px-4 lg:grid-cols-3 lg:px-6">
+            <Card className="@container/card lg:col-span-2">
+              <CardHeader>
+                <CardTitle>Total Visitors</CardTitle>
+                <CardDescription>
+                  <span className="hidden @[540px]/card:block">
+                    Total for the last 3 months
+                  </span>
+                  <span className="@[540px]/card:hidden">Last 3 months</span>
+                </CardDescription>
+              </CardHeader>
+              <CardContent className="px-2 pt-4 sm:px-6 sm:pt-6">
+                <Table className="w-full">
+                  <TableCaption>A list of your recent invoices.</TableCaption>
+                  <TableHeader>
+                    <TableRow>
+                      <TableHead className="w-[100px]">Invoice</TableHead>
+                      <TableHead>Status</TableHead>
+                      <TableHead>Method</TableHead>
+                      <TableHead className="text-right">Amount</TableHead>
+                    </TableRow>
+                  </TableHeader>
+                  <TableBody>
+                    {invoices.map((invoice) => (
+                      <TableRow key={invoice.invoice}>
+                        <TableCell className="font-medium">
+                          {invoice.invoice}
+                        </TableCell>
+                        <TableCell>{invoice.paymentStatus}</TableCell>
+                        <TableCell>{invoice.paymentMethod}</TableCell>
+                        <TableCell className="text-right">
+                          {invoice.totalAmount}
+                        </TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                  <TableFooter>
+                    <TableRow>
+                      <TableCell colSpan={3}>Total</TableCell>
+                      <TableCell className="text-right">$2,500.00</TableCell>
+                    </TableRow>
+                  </TableFooter>
+                </Table>
+              </CardContent>
+            </Card>
+            <Card className="@container/card lg:col-span-1">
+              <CardHeader>
+                <CardDescription>Growth Rate</CardDescription>
+                <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
+                  4.5%
+                </CardTitle>
+                <CardAction>
+                  <Badge variant="outline">
+                    <TrendingUp />
+                    +4.5%
+                  </Badge>
+                </CardAction>
+              </CardHeader>
+              <CardFooter className="flex-col items-start gap-1.5 text-sm">
+                <div className="line-clamp-1 flex gap-2 font-medium">
+                  Steady performance increase <TrendingUp className="size-4" />
+                </div>
+                <div className="text-muted-foreground">
+                  Meets growth projections
+                </div>
+              </CardFooter>
+            </Card>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
