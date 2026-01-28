7161e7b10f79808c5568563572a517492ba1d0dc
Abhishek Survase <abhishek.survase2000@gmail.com>
2026-01-29 02:55:04 +0530

    implemented Recent Admissions Card in Dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import { Button } from "@/components/ui/button";
 import { Link } from "react-router";
 import {
@@ -48,52 +49,40 @@ import {
 } from "@/components/ui/table";
 import { Input } from "@/components/ui/input";
 import { ModeToggle } from "@/components/mode-toggle.tsx";
+import axios from "axios";
+import { differenceInDays, differenceInMonths } from "date-fns";
 
 function Dashboard() {
-  const invoices = [
-    {
-      invoice: "INV001",
-      paymentStatus: "Paid",
-      totalAmount: "$250.00",
-      paymentMethod: "Credit Card",
-    },
-    {
-      invoice: "INV002",
-      paymentStatus: "Pending",
-      totalAmount: "$150.00",
-      paymentMethod: "PayPal",
-    },
-    {
-      invoice: "INV003",
-      paymentStatus: "Unpaid",
-      totalAmount: "$350.00",
-      paymentMethod: "Bank Transfer",
-    },
-    {
-      invoice: "INV004",
-      paymentStatus: "Paid",
-      totalAmount: "$450.00",
-      paymentMethod: "Credit Card",
-    },
-    {
-      invoice: "INV005",
-      paymentStatus: "Paid",
-      totalAmount: "$550.00",
-      paymentMethod: "PayPal",
-    },
-    {
-      invoice: "INV006",
-      paymentStatus: "Pending",
-      totalAmount: "$200.00",
-      paymentMethod: "Bank Transfer",
-    },
-    {
-      invoice: "INV007",
-      paymentStatus: "Unpaid",
-      totalAmount: "$300.00",
-      paymentMethod: "Credit Card",
-    },
-  ];
+  const [latestStudents, fetchLatestStudents] = useState([
+    { id: "pre", date_of_admission: `${new Date()}` },
+  ]);
+  useEffect(() => {
+    const fetchAllStudentData = async () => {
+      try {
+        const apiUrl = `http://localhost:8810/all_active_students`;
+        // console.log(paginationData);
+        const response = await axios.get(apiUrl, {
+          params: {
+            page: 0,
+            limit: 5,
+          },
+        });
+        fetchLatestStudents(response.data.data);
+        // console.log(response.data);
+      } catch (error) {
+        console.log(error);
+        // setError(error.message);
+      } finally {
+        // setLoading(false);
+        console.log(new Date().getFullYear());
+        console.log(
+          `Last Admission was ${latestStudents[0].date_of_admission - new Date() / 1000 / 60 / 24} months ago`,
+        );
+      }
+    };
+    fetchAllStudentData();
+  }, []);
+
   return (
     <div className="@container/main flex-1 flex flex-col">
       <header className="h-16 --background border-b flex items-center justify-between px-8 sticky top-0 z-10">
@@ -217,45 +206,49 @@ function Dashboard() {
           <div className="grid grid-cols-1 gap-4 px-4 lg:grid-cols-3 lg:px-6">
             <Card className="@container/card lg:col-span-2">
               <CardHeader>
-                <CardTitle>Total Visitors</CardTitle>
+                <CardTitle>Recent Admissions</CardTitle>
                 <CardDescription>
                   <span className="hidden @[540px]/card:block">
-                    Total for the last 3 months
+                    {`Last Admission was ${differenceInDays(new Date(), new Date(latestStudents[0].date_of_admission))} day/s ago`}
                   </span>
                   <span className="@[540px]/card:hidden">Last 3 months</span>
                 </CardDescription>
               </CardHeader>
               <CardContent className="px-2 pt-4 sm:px-6 sm:pt-6">
                 <Table className="w-full">
-                  <TableCaption>A list of your recent invoices.</TableCaption>
+                  <TableCaption>A list of recent admissions.</TableCaption>
                   <TableHeader>
                     <TableRow>
-                      <TableHead className="w-[100px]">Invoice</TableHead>
-                      <TableHead>Status</TableHead>
-                      <TableHead>Method</TableHead>
-                      <TableHead className="text-right">Amount</TableHead>
+                      <TableHead className="w-[100px]">Admission No</TableHead>
+                      <TableHead>First Name</TableHead>
+                      <TableHead>Last Name</TableHead>
+                      <TableHead>Gender</TableHead>
+                      <TableHead>Contact</TableHead>
+                      <TableHead className="text-right">
+                        Admission Date
+                      </TableHead>
                     </TableRow>
                   </TableHeader>
                   <TableBody>
-                    {invoices.map((invoice) => (
-                      <TableRow key={invoice.invoice}>
+                    {latestStudents.map((student) => (
+                      <TableRow key={student.id}>
                         <TableCell className="font-medium">
-                          {invoice.invoice}
+                          {student.admission_no}
                         </TableCell>
-                        <TableCell>{invoice.paymentStatus}</TableCell>
-                        <TableCell>{invoice.paymentMethod}</TableCell>
+                        <TableCell>{student.first_name}</TableCell>
+                        <TableCell>{student.last_name}</TableCell>
+                        <TableCell>{student.gender}</TableCell>
+                        <TableCell>{student.contact_number}</TableCell>
                         <TableCell className="text-right">
-                          {invoice.totalAmount}
+                          {
+                            new Date(student.date_of_admission)
+                              .toISOString()
+                              .split("T")[0]
+                          }
                         </TableCell>
                       </TableRow>
                     ))}
                   </TableBody>
-                  <TableFooter>
-                    <TableRow>
-                      <TableCell colSpan={3}>Total</TableCell>
-                      <TableCell className="text-right">$2,500.00</TableCell>
-                    </TableRow>
-                  </TableFooter>
                 </Table>
               </CardContent>
             </Card>
