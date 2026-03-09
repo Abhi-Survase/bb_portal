ccf6adaeac4b556a60eefd436131bf5d790045e5
Abhishek Survase <abhishek.survase2000@gmail.com>
2026-03-10 01:39:53 +0530

    added routing to ui-Dashboard API

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import { student_metadata_db } from "./config/db.js";
 import logger from "./utils/logger.js";
+import uiDashboardRoutes from "./routes/uiDashboardRoutes.js";
 
 const app = express();
 
@@ -11,10 +12,6 @@ app.use(
     origin: "http://localhost:5173", // only allow this origin
   }),
 );
-
-app.get("/", (req, res) => {
-  throw new Error("Incorrect Endpoint! Check API Documentation.");
-});
 
 app.get("/favicon.ico", (req, res) => {
   return res.status(204).end;
@@ -70,77 +67,7 @@ app.get("/all_active_students", async (req, res) => {
   }
 });
 
-app.get("/ui_dashboard/list", async (req, res) => {
-  try {
-    logger.info("ui_dashboard/list | fetching data");
-    const latestAdmissionsListQuery =
-      "SELECT id, admission_no, first_name, last_name, gender, contact_number, date_of_admission FROM school_metadata.students WHERE is_active = 'true' ORDER BY date_of_admission desc, id asc LIMIT 5";
-    const latestAdmissionsListResult = await student_metadata_db.query(
-      latestAdmissionsListQuery,
-    );
-    logger.info(
-      "ui_dashboard/list | Response | latestAdmissionsListResult => " +
-        JSON.stringify(latestAdmissionsListResult[0]),
-    );
-    return res.status(200).json({
-      data: {
-        latestAdmissionsList: latestAdmissionsListResult[0],
-      },
-    });
-    // setTimeout(() => {
-    //   logger.info(
-    //     "ui_dashboard/list | DELAYED Response | latestAdmissionsListResult =>",
-    //     JSON.stringify(latestAdmissionsListResult[0]),
-    //   );
-    //   res.status(200).json({
-    //     data: {
-    //       latestAdmissionsList: latestAdmissionsListResult[0],
-    //     },
-    //   });
-    // }, 2000);
-  } catch (err) {
-    logger.error("ui_dashboard/list | Exception =>> " + err);
-    return res.status(500).json({
-      error: "Something Went Wrong",
-      code: err.errno,
-    });
-  }
-});
-
-app.get("/ui_dashboard/summary", async (req, res) => {
-  try {
-    logger.info("ui_dashboard/summary | fetching data");
-    const newAdmissionCountQuery =
-      "SELECT count(id) as new_admissions_count FROM school_metadata.students WHERE date_of_admission > SUBDATE(sysdate(), INTERVAL 1 MONTH) AND is_active = 'true'";
-    const totalStudentCountQuery =
-      "SELECT count(*) as total_count FROM school_metadata.students WHERE is_active = 'true'";
-    const [newAdmissionCountResult, totalStudentCountResult] =
-      await Promise.all([
-        student_metadata_db.query(newAdmissionCountQuery),
-        student_metadata_db.query(totalStudentCountQuery),
-      ]);
-    logger.info(
-      "ui_dashboard/summary | Response | totalStudentCountResult =>" +
-        JSON.stringify(totalStudentCountResult[0]),
-    );
-    logger.info(
-      "ui_dashboard/summary | Response | newAdmissionCountResult =>" +
-        JSON.stringify(newAdmissionCountResult[0]),
-    );
-    return res.status(200).json({
-      data: {
-        newAdmissionCount: newAdmissionCountResult[0],
-        totalStudentCount: totalStudentCountResult[0],
-      },
-    });
-  } catch (err) {
-    logger.error("ui_dashboard/summary | Exception =>> " + err);
-    return res.status(500).json({
-      error: "Something Went Wrong",
-      code: err.errno,
-    });
-  }
-});
+app.use("/ui_dashboard", uiDashboardRoutes);
 
 app.get("/get_student_byAdmissionNo/:admission_no", async (req, res) => {
   try {
